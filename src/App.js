beb0d04b647a49b47bac33df575d1d2821e95158
Jim2510 <gmguglielmino@gmail.com>
2023-10-09 13:10:28 +0200

    implementazione funzioni pulsanti

--- src/App.js
+++ src/App.js
@@ -7,17 +7,50 @@ import dynamite from "./images/dynamite.png";
 import philadelphia from "./images/philadelphia.png";
 import rainbow from "./images/rainbow.png";
 import shrimp from "./images/shrimp.png";
+import '../src/App.css'
 
 class App extends Component {
   state = {
     cards: [
-      { id: 0, nome: "California", prezzo: 1.99, immagine: california, quantità: 0 },
+      {
+        id: 0,
+        nome: "California",
+        prezzo: 1.99,
+        immagine: california,
+        quantità: 0,
+      },
       { id: 1, nome: "Dragon", prezzo: 1.49, immagine: dragon, quantità: 0 },
-      { id: 2, nome: "Dynamite", prezzo: 2.49, immagine: dynamite, quantità: 0 },
-      { id: 3, nome: "Philadelphia", prezzo: 1.49, immagine: philadelphia, quantità: 0 },
+      {
+        id: 2,
+        nome: "Dynamite",
+        prezzo: 2.49,
+        immagine: dynamite,
+        quantità: 0,
+      },
+      {
+        id: 3,
+        nome: "Philadelphia",
+        prezzo: 1.49,
+        immagine: philadelphia,
+        quantità: 0,
+      },
       { id: 4, nome: "Rainbow", prezzo: 2.99, immagine: rainbow, quantità: 0 },
       { id: 5, nome: "Shrimp", prezzo: 3.49, immagine: shrimp, quantità: 0 },
     ],
+  };
+
+  handleReset = () => {
+    // Ripristina lo stato iniziale
+    this.setState({
+      cards: [
+        { id: 0, nome: "California", prezzo: 1.99, immagine: california, quantità: 0 },
+        { id: 1, nome: "Dragon", prezzo: 1.49, immagine: dragon, quantità: 0 },
+        { id: 2, nome: "Dynamite", prezzo: 2.49, immagine: dynamite, quantità: 0 },
+        { id: 3, nome: "Philadelphia", prezzo: 1.49, immagine: philadelphia, quantità: 0 },
+        { id: 4, nome: "Rainbow", prezzo: 2.99, immagine: rainbow, quantità: 0 },
+        { id: 5, nome: "Shrimp", prezzo: 3.49, immagine: shrimp, quantità: 0 },
+      ],
+    });
   };
 
   handleDelete = (cardId) => {
@@ -25,13 +58,23 @@ class App extends Component {
     this.setState({ cards });
   };
 
-  handleIncrement = card => {
+  handleIncrement = (card) => {
     const cards = [...this.state.cards];
     const id = cards.indexOf(card);
-    cards[id] = {...card};
-    cards[id].quantità++
-    this.setState({cards})
-  }
+    cards[id] = { ...card };
+    cards[id].quantità++;
+    this.setState({ cards });
+  };
+
+  handleDecrement = (card) => {
+    const cards = [...this.state.cards];
+    const id = cards.indexOf(card);
+    if (cards[id].quantità > 0) {
+      cards[id] = { ...card };
+      cards[id].quantità--;
+      this.setState({ cards });
+    }
+  };
 
   render() {
     return (
@@ -42,12 +85,20 @@ class App extends Component {
           <hr />
           <div className="row d-flex justify-content-center">
             {this.state.cards.map((card) => (
-              <Card 
-              key={card.id} 
-              card={card} 
-              onIncrement={this.handleIncrement}
-              onDelete={this.handleDelete} />
+              <div key={card.id} className="col-md-4">
+                <Card
+                  card={card}
+                  onIncrement={this.handleIncrement}
+                  onDecrement={this.handleDecrement}
+                  onDelete={this.handleDelete} // Passa la funzione di decremento
+                />
+              </div>
             ))}
+          </div>
+          <div className="text-right">
+            <button className="btn btn-warning" onClick={this.handleReset}>
+              Reset
+            </button>
           </div>
         </div>
       </>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,25 +1,45 @@
-import { Component } from 'react';
+import React, { Component } from "react";
 
 class Card extends Component {
-    render() {
-  return (
-    <div className="card" style={{width: '18rem', textAlign: 'center'}}>
-        <button onClick={() => this.props.onIncrement(this.props.card)} className="btn btn-primary">
-          Aggiungi <span className='badge badge-light'>{this.props.card.quantità}</span>
-        </button>
-      <img src={this.props.card.immagine} className="card-img-top" alt="..." />
-      <div className="card-body">
-        <h5 className="card-title">{this.props.card.nome} Roll</h5>
-        <p className="card-text">
-          €{this.props.card.prezzo}
-        </p>
-        <button onClick={() => this.props.onDelete(this.props.card.id)} className="btn btn-outline-danger">
-          Elimina
-        </button>
+  render() {
+    return (
+      <div className="card mb-4">
+        <img
+          src={this.props.card.immagine}
+          className="card-img-top"
+          alt={this.props.card.nome}
+        />
+        <div className="card-body">
+          <h5 className="card-title">{this.props.card.nome} Roll</h5>
+          <p className="card-text">Prezzo: {this.props.card.prezzo} €</p>
+          <p className="card-text">Quantità: {this.props.card.quantità}</p>
+          <button
+            onClick={() => this.props.onIncrement(this.props.card)}
+            className="btn btn-primary"
+          >
+            Aggiungi{" "}
+            {/* <span className="badge badge-light">
+              {this.props.card.quantità}
+            </span> */}
+          </button>
+          {this.props.card.quantità > 0 && ( // Mostra il pulsante "Rimuovi" solo se la quantità è maggiore di 0
+            <button
+              onClick={() => this.props.onDecrement(this.props.card)}
+              className="btn btn-danger"
+            >
+              Rimuovi
+            </button>
+          )}
+          <button
+            onClick={() => this.props.onDelete(this.props.card.id)}
+            className="btn btn-outline-danger"
+          >
+            Elimina
+          </button>
+        </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
 }
 
-export default Card
+export default Card;
