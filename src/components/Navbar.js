0273da42f1f33512842dd68f342ed7078b74e0dc
Jim2510 <gmguglielmino@gmail.com>
2023-10-29 20:28:50 +0100

    Aggiunta logo navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,11 @@
+import logo from '../images/R.png'
+
+
 function Navbar() {
   return (
-    <nav className="navbar navbar-dark bg-dark">
-      <div className="container-fluid">
+    <nav className="navbar navbar-dark bg-dark ">
+      <div className="container-fluid justify-content-start ml-5">
+        <img src={logo} alt="risto sushi" className="navbar-brand" style={{width:"50px", height:"60px"}} />
         <span className="navbar-brand mb-0 h1">Risto Sushi</span>
       </div>
     </nav>
